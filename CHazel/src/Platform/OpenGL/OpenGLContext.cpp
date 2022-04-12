9aee97b77ec5c4802945373404811a46442e485a
dugulanhun <dugulanhun@163.com>
2022-04-12 17:25:29 +0800

    First triangle.

--- CHazel/src/Platform/OpenGL/OpenGLContext.cpp
+++ CHazel/src/Platform/OpenGL/OpenGLContext.cpp
@@ -18,6 +18,11 @@ namespace CHazel {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		CHZ_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		CHZ_CORE_INFO("OpenGL Info:");
+		CHZ_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		CHZ_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		CHZ_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
