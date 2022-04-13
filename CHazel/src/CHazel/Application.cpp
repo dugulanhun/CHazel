c045fbd9a3a6d62c62350e0f67c3e09119f131ad
dugulanhun <dugulanhun@163.com>
2022-04-13 20:33:42 +0800

    Add buffer layout descriptors for vertex buffers.

--- CHazel/src/CHazel/Application.cpp
+++ CHazel/src/CHazel/Application.cpp
@@ -11,6 +11,27 @@ namespace CHazel{
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case CHazel::ShaderDataType::Float:    return GL_FLOAT;
+			case CHazel::ShaderDataType::Float2:   return GL_FLOAT;
+			case CHazel::ShaderDataType::Float3:   return GL_FLOAT;
+			case CHazel::ShaderDataType::Float4:   return GL_FLOAT;
+			case CHazel::ShaderDataType::Mat3:     return GL_FLOAT;
+			case CHazel::ShaderDataType::Mat4:     return GL_FLOAT;
+			case CHazel::ShaderDataType::Int:      return GL_INT;
+			case CHazel::ShaderDataType::Int2:     return GL_INT;
+			case CHazel::ShaderDataType::Int3:     return GL_INT;
+			case CHazel::ShaderDataType::Int4:     return GL_INT;
+			case CHazel::ShaderDataType::Bool:     return GL_BOOL;
+		}
+
+		CHZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		CHZ_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -25,16 +46,31 @@ namespace CHazel{
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ShaderDataType::Float3, "a_Position"},
+				{ShaderDataType::Float4, "a_Color"}
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, element.GetComponentCount(), ShaderDataTypeToOpenGLBaseType(element.Type), element.Normalized ? GL_TRUE : GL_FALSE, layout.GetStride(), (const void*)element.Offset);
+			index++;
+		}
 
 		uint32_t indices[3] = { 0, 1, 2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
--- CHazel/src/Platform/OpenGL/OpenGLBuffer.h
+++ CHazel/src/Platform/OpenGL/OpenGLBuffer.h
@@ -13,8 +13,13 @@ namespace CHazel
 
 		virtual void Bind() const override;
 		virtual void Unbind() const override;
+
+		const BufferLayout& GetLayout() const override { return m_Layout; }
+		void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
+
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer :public IndexBuffer
