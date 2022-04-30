9af4a0268f89b3fe40bb11d9dab8f5cc4e1ed09a
dugulanhun <dugulanhun@163.com>
2022-05-01 07:37:39 +0800

    Moved OpenGL shader implementation to OpenGLShader class + added more uniform functions.

--- CHazel/src/CHazel/Renderer/Renderer.cpp
+++ CHazel/src/CHazel/Renderer/Renderer.cpp
@@ -1,6 +1,7 @@
 #include "chzpch.h"
 #include "Renderer.h"
 
+#include "Platform/OpenGL/OpenGLShader.h"
 
 namespace CHazel {
 
@@ -19,8 +20,8 @@ namespace CHazel {
 	void Renderer::Submit(const std::shared_ptr <Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
 	{
 		shader->Bind();
-		shader->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
-		shader->UploadUniformMat4("u_Transform", transform);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RendererCommand::DrawIndexed(vertexArray);
--- SandBox/src/SandBoxApp.cpp
+++ SandBox/src/SandBoxApp.cpp
@@ -1,8 +1,12 @@
 #include <CHazel.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer: public CHazel::Layer
 {
@@ -80,9 +84,9 @@ public:
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
 			}
 		)";
-		m_Shader.reset(new CHazel::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(CHazel::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
@@ -98,18 +102,21 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 			in vec3 v_Position;
+
+			uniform vec3 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_BlueShader.reset(new CHazel::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(CHazel::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 
 	}
 
@@ -140,13 +147,16 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		std::dynamic_pointer_cast<CHazel::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<CHazel::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11, y * 0.11, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				CHazel::Renderer::Submit(m_BlueShader, m_SquareVertexArray, transform);
+				CHazel::Renderer::Submit(m_FlatColorShader, m_SquareVertexArray, transform);
 			}
 		}
 
@@ -159,6 +169,9 @@ public:
 
 	void OnImGuiRender() override
 	{ 	
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 	
 	void OnEvent(CHazel::Event& event) override
@@ -168,7 +181,7 @@ private:
 	std::shared_ptr<CHazel::Shader> m_Shader;
 	std::shared_ptr<CHazel::VertexArray> m_VertexArray;
 
-	std::shared_ptr<CHazel::Shader> m_BlueShader;
+	std::shared_ptr<CHazel::Shader> m_FlatColorShader;
 	std::shared_ptr<CHazel::VertexArray> m_SquareVertexArray;
 
 	CHazel::OrthographicCameca m_Camera;
@@ -177,6 +190,8 @@ private:
 
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 180.0f;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 
 };
 
