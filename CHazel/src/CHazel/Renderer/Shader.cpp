0e07428c69834d2888595ba76613c46882361dab
dugulanhun <dugulanhun@163.com>
2022-05-02 01:34:34 +0800

    Shaders can now be read from files.

--- CHazel/src/CHazel/Renderer/Shader.cpp
+++ CHazel/src/CHazel/Renderer/Shader.cpp
@@ -7,6 +7,18 @@
 
 namespace CHazel {
 
+	CHazel::Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (RendererAPI::GetAPI())
+		{
+		case RendererAPI::API::None: CHZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL: return new OpenGLShader(filepath);
+		}
+
+		CHZ_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSource, const std::string& fragmentSource)
 	{
 		switch (RendererAPI::GetAPI())
